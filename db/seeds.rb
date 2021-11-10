d95374c91c833aa99e33971347c8bd1ef310506c
Kyung Ho Shin <davidshin0413@gmail.com>
2021-11-10 16:31:53 -0500

    almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(name: "David's", address: '1360 Decarie St', phone_number: '514-561-1314', category: 'japanese')
+Restaurant.create(name: "Joon's", address: '2678 Ernest-Hemingway', phone_number: '514-111-1111', category: 'chinese')
+Restaurant.create(name: "Leo's", address: '1234 random st', phone_number: '514-222-2222', category: 'french')
+Restaurant.create(name: "Zach's", address: '5678 fake st', phone_number: '514-333-3333', category: 'italian')
+Restaurant.create(name: "alex's", address: '1324 bullshit st', phone_number: '514-444-4444', category: 'belgian')
